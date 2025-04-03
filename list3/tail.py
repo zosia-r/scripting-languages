0db737ae4130acb123e2b1e2c517af7a7013ee4e
Sunariz <113854410+Sunariz@users.noreply.github.com>
2025-04-03 23:54:40 +0200

    Update tail.py

--- list3/tail.py
+++ list3/tail.py
@@ -1,32 +1,54 @@
-Uproszczona implementacja tail -f
-1 import time,sys,os
-2
-3 def tail(filename):
-4 with open(filename, "r") as file:
-5 # znajdujemy początkową pozycję pliku
-6 file.seek(0)
-7 while True:
-8 # zapisujemy aktualną pozycję pliku
-9 current_position = file.tell()
-10 line = file.readline()
-11 if not line:
-12 # jeśli nie ma nowych linii, czekamy chwilę
-13 time.sleep(0.1)
-14
-15 # weryfikujemy rozmiar pliku
-16 current_size = os.stat(filename).st_size
-17
-18 # jeśli obecna pozycja jest dalej,
-19 # niż rozmiar pliku, przesuwamy na początek
-20 if current_position > current_size:
-21 file.seek(0)
-22 else:
-23 # jeśli mamy nową linię, wypisujemy ją na ekran
-24 sys.stdout.write(line)
-25 sys.stdout.flush()
-26
-27 if __name__ == "__main__":
-28 try:
-29 tail(sys.argv[1])
-30 except IndexError:
-31 print("Please provide first argument.")
+import sys,time,os,io
+from collections import deque
+
+sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+def process_lines(source, n):
+    lines = deque(source, maxlen=n)
+    for line in lines:
+        sys.stdout.write(line)
+    sys.stdout.flush()
+
+def follow(file):
+    while True:
+        line = file.readline()
+        if line:
+            sys.stdout.write(line)
+            sys.stdout.flush()
+        else:
+            time.sleep(0.1)  # Czekamy na nowe linie
+
+def tail(filename, n, to_follow):
+    try:
+        with open(filename, "r", encoding='utf-8') as file:
+            process_lines(file, n)  # Wyświetlamy ostatnie n linii 
+            if to_follow: follow(file)
+    except Exception as e:
+        print(f"Error: {e}")
+
+def parse_arguments():
+    filename = None
+    n = 10
+    to_follow = False
+    
+    for arg in sys.argv[1:]:
+        if arg.startswith("--lines="):
+            try:
+                n = int(arg.split("=")[1])
+            except ValueError:
+                print("Invalid number of lines.")
+                sys.exit(1)
+        elif arg == "--follow":
+            to_follow = True
+        else:
+            filename = arg
+    return filename, n, to_follow
+
+if __name__ == "__main__":
+    filename, n, to_follow = parse_arguments()
+
+    if filename:
+        tail(filename, n, to_follow)
+    else:
+        process_lines(sys.stdin, n)
