c4867c8182ef8122a34f15e71abfa8f985fe916f
Zofia Różańska <280526@student.pwr.edu.pl>
2025-05-06 13:13:46 +0200

    add: ex4

---
+++ list6/SeriesValidator.py
@@ -0,0 +1,88 @@
+from TimeSeries import TimeSeries
+
+import abc
+from typing import List
+
+
+
+class SeriesValidator(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def analyze(self, series: TimeSeries) -> List[str]:
+        '''' Returns a list of strings with detected anomalies. '''
+        pass
+
+
+
+class OutlierDetector(SeriesValidator):
+    def __init__(self, threshold: float = 2.0):
+        self.threshold = threshold
+
+
+    def analyze(self, series: TimeSeries) -> List[str]:
+        stddev = series.stddev
+        mean = series.mean
+
+        if stddev is None or mean is None:
+            return ['No data available for analysis.']
+        
+        messages = []
+        for pair in series[:]:
+            if abs(pair[1] - mean) > self.threshold * stddev:
+                messages.append(f'Outlier detected at {pair[0]}: {pair[1]}')
+        return messages
+    
+
+
+class ZeroSpikeDetector(SeriesValidator):
+    def __init__(self, threshold: float = 3.0):
+        self.threshold = threshold
+
+
+    def analyze(self, series: TimeSeries) -> List[str]:
+        messages = []
+        count = 0
+
+        for pair in series[:]:
+            if pair[1] == 0 or pair[1] == None:
+                count += 1
+                if count >= self.threshold:
+                    messages.append(f'Threshold of {int(self.threshold)} zero spikes exceeded at {pair[0]}')
+            else:
+                count = 0
+        
+        return messages
+    
+
+
+class ThresholdDetector(SeriesValidator):
+    def __init__(self, threshold: float = 10.0):
+        self.threshold = threshold
+
+    def analyze(self, series: TimeSeries) -> List[str]:
+        messages = []
+
+        for pair in series[:]:
+            if pair[1] > self.threshold:
+                messages.append(f'Threshold of {self.threshold} exceeded at {pair[0]}: {pair[1]}')
+        
+        return messages
+    
+
+if __name__ == '__main__':
+    # Example usage
+    from datetime import datetime
+    ts = TimeSeries('Temperature', 'Station1', 1.0, 
+                    [datetime(2023, 1, 1), datetime(2023, 1, 2), 
+                     datetime(2023, 1, 3), datetime(2023, 1, 4), 
+                     datetime(2023, 1, 5), datetime(2023, 1, 6)],
+                    [10000.0, 15.0, 20.0, 0.0, 0.0, 35.0], 'C')
+            
+ 
+    outlier_detector = OutlierDetector(threshold=2.0)
+    print(outlier_detector.analyze(ts))
+
+    zero_spike_detector = ZeroSpikeDetector(threshold=2.0)
+    print(zero_spike_detector.analyze(ts))
+
+    threshold_detector = ThresholdDetector(threshold=10.0)
+    print(threshold_detector.analyze(ts))
